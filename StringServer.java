8d25ad4f99069453ba42fdaa9084cbe9ae6ab16a
LeenaJannatipour <96517628+LeenaJannatipour@users.noreply.github.com>
2023-05-02 11:43:06 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -1,1 +1,15 @@
+//should keep track of a single string that gets added to by incoming requests
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
+class StringHandler implements URLHandler {
+  List<String> lines;
+  String path;
+  StringHandler(String path) throws IOException {
+    this.path = path;
+    this.lines = Files.readAllLines(Paths.get(path));
+  }
+  
