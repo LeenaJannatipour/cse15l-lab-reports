4a6f3a463cd4c0cb2b40fc1f16fdb4b87f04c7b9
LeenaJannatipour <ljannati@ucsd.edu>
2023-05-10 19:54:51 -0700

    ykwtgo

--- StringServer.java
+++ StringServer.java
@@ -1,15 +1,41 @@
-//should keep track of a single string that gets added to by incoming requests
 import java.io.IOException;
 import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 
-class StringHandler implements URLHandler {
-  List<String> lines;
-  String path;
-  StringHandler(String path) throws IOException {
-    this.path = path;
-    this.lines = Files.readAllLines(Paths.get(path));
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+class Handler implements URLHandler {
+  // The one bit of state on the server: a number that will be manipulated by
+  // various requests.
+  //int num = 0;
+  String path = "";
+  public String handleRequest(URI url) {
+    System.out.println(url);
+      if (url.getPath().equals("/add-message")) {
+        System.out.println("IT WORKED");
+        String query = url.getQuery();
+        if (query.startsWith("s=")) {
+          String message = query.substring(2);
+          path += "\n" + message;
+          return path;
+        } else { return "Invalid query parameter";}
+      }
+      else { return "404 Not Found"; }
   }
+}
+
+public class StringServer {
   
+  public static void main(String[] args) throws IOException {
+    if(args.length == 0){
+        System.out.println("Missing port number! Try any number between 1024 to 49151");
+        return;
+    }
+
+    int port = Integer.parseInt(args[0]);
+
+    Server.start(port, new Handler());
+}
+  
+}
--- lab-report-2.md
+++ lab-report-2.md
@@ -1,2 +1,10 @@
 # Lab Report 2
 
+
+
+
+## Part 2 - Lab 3 Bug
+
+
+
+## Part 3 - Week 3 Lab Reflection
